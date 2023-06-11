c2cb16bb3c73fac640ff60e7a7299b144e876642
João Bolsson <joaobolsson@animati.com.br>
2023-06-11 15:09:34 -0300

    cases of Bolsa validated

--- src/test/java/br/com/efono/model/KnownCaseTest.java
+++ src/test/java/br/com/efono/model/KnownCaseTest.java
@@ -192,7 +192,7 @@ public class KnownCaseTest {
 
         // vou testar palavra por palavra e ir avançando pra ver o resultado
         List<String> enableWords = Arrays.asList(new String[]{"Anel", "Barriga", "Batom", "Bebê", "Beijo", "Biblioteca", 
-            "Bicicleta"});
+            "Bicicleta", "Bolsa"});
         enableWords.forEach(w -> mapCases.put(w, new ArrayList<>()));
 
         // just reading the cases
