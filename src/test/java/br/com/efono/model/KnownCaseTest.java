5903d1437816f5b026026df1753fce495999f7f3
João Bolsson <joaobolsson@animati.com.br>
2023-06-11 15:32:28 -0300

    cases of Calça validated

--- src/test/java/br/com/efono/model/KnownCaseTest.java
+++ src/test/java/br/com/efono/model/KnownCaseTest.java
@@ -192,7 +192,7 @@ public class KnownCaseTest {
 
         // vou testar palavra por palavra e ir avançando pra ver o resultado
         List<String> enableWords = Arrays.asList(new String[]{"Anel", "Barriga", "Batom", "Bebê", "Beijo", "Biblioteca", 
-            "Bicicleta", "Bolsa", "Brinco", "Bruxa", "Cabelo", "Cachorro", "Caixa"});
+            "Bicicleta", "Bolsa", "Brinco", "Bruxa", "Cabelo", "Cachorro", "Caixa", "Calça"});
         enableWords.forEach(w -> mapCases.put(w, new ArrayList<>()));
 
         // just reading the cases
