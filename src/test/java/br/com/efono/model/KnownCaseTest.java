bece54e846433ff395c3bd6ab838a4fcd2a5e547
João Bolsson <joaobolsson@animati.com.br>
2023-06-11 15:45:24 -0300

    cases of Caminhao validated

--- src/test/java/br/com/efono/model/KnownCaseTest.java
+++ src/test/java/br/com/efono/model/KnownCaseTest.java
@@ -192,7 +192,7 @@ public class KnownCaseTest {
 
         // vou testar palavra por palavra e ir avançando pra ver o resultado
         List<String> enableWords = Arrays.asList(new String[]{"Anel", "Barriga", "Batom", "Bebê", "Beijo", "Biblioteca", 
-            "Bicicleta", "Bolsa", "Brinco", "Bruxa", "Cabelo", "Cachorro", "Caixa", "Calça", "Cama"});
+            "Bicicleta", "Bolsa", "Brinco", "Bruxa", "Cabelo", "Cachorro", "Caixa", "Calça", "Cama", "Caminhão"});
         enableWords.forEach(w -> mapCases.put(w, new ArrayList<>()));
 
         // just reading the cases
