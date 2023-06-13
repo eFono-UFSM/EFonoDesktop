dc7f00f913d30f6810604a9dac006147162d467b
João Bolsson <joaobolsson@animati.com.br>
2023-06-13 07:47:05 -0300

    cases of Espelho validated

--- src/test/java/br/com/efono/model/KnownCaseTest.java
+++ src/test/java/br/com/efono/model/KnownCaseTest.java
@@ -194,7 +194,7 @@ public class KnownCaseTest {
         List<String> enableWords = Arrays.asList(new String[]{"Anel", "Barriga", "Batom", "Bebê", "Beijo", "Biblioteca", 
             "Bicicleta", "Bolsa", "Brinco", "Bruxa", "Cabelo", "Cachorro", "Caixa", "Calça", "Cama", "Caminhão", "Casa", 
             "Cavalo", "Chapéu", "Chiclete", "Chifre", "Chinelo", "Cobra", "Coelho", "Colher", "Cruz", "Dado", "Dedo", 
-            "Dente", "Dragão", "Escrever"});
+            "Dente", "Dragão", "Escrever", "Espelho"});
         enableWords.forEach(w -> mapCases.put(w, new ArrayList<>()));
 
         // just reading the cases
