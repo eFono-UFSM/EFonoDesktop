6c87bfddc689ade6fb7f769069175d942efd7c2f
João Bolsson <joaobolsson@animati.com.br>
2023-06-17 13:28:13 -0300

    cases of Jornal validated

--- src/test/java/br/com/efono/model/KnownCaseTest.java
+++ src/test/java/br/com/efono/model/KnownCaseTest.java
@@ -262,8 +262,10 @@ public class KnownCaseTest {
             "Bicicleta", "Bolsa", "Brinco", "Bruxa", "Cabelo", "Cachorro", "Caixa", "Calça", "Cama", "Caminhão", "Casa",
             "Cavalo", "Chapéu", "Chiclete", "Chifre", "Chinelo", "Cobra", "Coelho", "Colher", "Cruz", "Dado", "Dedo",
             "Dente", "Dragão", "Escrever", "Espelho", "Estrela", "Faca", "Flor", "Floresta", "Fogo", "Folha", "Fralda",
-            "Fruta", "Galinha", "Garfo", "Gato", "Girafa", "Grama", "Gritar", "Igreja", "Jacaré"});
+            "Fruta", "Galinha", "Garfo", "Gato", "Girafa", "Grama", "Gritar", "Igreja", "Jacaré", "Jornal"});
         enableWords.forEach(w -> mapCases.put(w, new ArrayList<>()));
+        
+        // TODO: ʒoɾnal, essa transcrição está correta?
 
         // just reading the cases
         for (KnownCase c : casesFromCSV) {
