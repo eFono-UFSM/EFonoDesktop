f1a330877d28b451da7ed11ace66609325c9a820
João Bolsson <joaobolsson@animati.com.br>
2023-06-18 17:00:03 -0300

    cases of Plastico validated

--- src/test/java/br/com/efono/model/KnownCaseTest.java
+++ src/test/java/br/com/efono/model/KnownCaseTest.java
@@ -264,7 +264,7 @@ public class KnownCaseTest {
             "Dente", "Dragão", "Escrever", "Espelho", "Estrela", "Faca", "Flor", "Floresta", "Fogo", "Folha", "Fralda",
             "Fruta", "Galinha", "Garfo", "Gato", "Girafa", "Grama", "Gritar", "Igreja", "Jacaré", "Jornal", "Lápis",
             "Letra", "Língua", "Livro", "Magro", "Mesa", "Microfone", "Nariz", "Navio", "Nuvem", "Passarinho", "Pastel",
-            "Pedra", "Placa"});
+            "Pedra", "Placa", "Plástico"});
         enableWords.forEach(w -> mapCases.put(w, new ArrayList<>()));
 
         /**
