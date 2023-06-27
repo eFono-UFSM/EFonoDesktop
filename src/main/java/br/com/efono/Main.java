d7ad24d2b30817a453417333d31db20048f2a957
João Bolsson <joaobolsson@animati.com.br>
2023-06-27 08:22:10 -0300

    running simulations with: hard/easy words first

--- src/main/java/br/com/efono/Main.java
+++ src/main/java/br/com/efono/Main.java
@@ -4,15 +4,13 @@ import br.com.efono.db.MongoConnection;
 import br.com.efono.db.MySQLConnection;
 import br.com.efono.model.Assessment;
 import br.com.efono.model.KnownCase;
-import br.com.efono.model.Phoneme;
+import br.com.efono.util.SimulationWordsSequence;
 import br.com.efono.util.Util;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -54,25 +52,6 @@ public class Main {
     }
 
     private static void processSimulation() throws SQLException {
-
-        /**
-         * The first words in the array are the most difficult, according with the statistics in our database. The
-         * number of wrong transcriptions of a word indicates the level of difficult.
-         */
-        final String[] words = new String[]{"Travesseiro", "Biblioteca", "Floresta", "Colher", "Microfone", "Vidro", "Pedra",
-            "Estrela", "Magro", "Dragão", "Escrever", "Flor", "Igreja", "Plástico", "Letra", "Bruxa", "Gritar",
-            "Bicicleta", "Chifre", "Chiclete", "Livro", "Presente", "Refri", "Fralda", "Fruta", "Placa", "Soprar",
-            "Trem", "Cruz", "Grama", "Zebra", "Cobra", "Prato", "Brinco", "Girafa", "Jacaré", "Garfo", "Jornal",
-            "Nariz", "Tesoura", "Passarinho", "Zero", "Ventilador", "Porta", "Relógio", "Nuvem", "Espelho", "Cachorro",
-            "Caixa", "Beijo", "Chinelo", "Língua", "Coelho", "Pastel", "Chapéu", "Folha", "Calça", "Barriga", "Sofá",
-            "Casa", "Bolsa", "Cavalo", "Gato", "Mesa", "Galinha", "Lápis", "Vaca", "Cabelo", "Sapato", "Sapo", "Fogo",
-            "Caminhão", "Faca", "Rabo", "Dente", "Tênis", "Terra", "Navio", "Dado", "Batom", "Cama", "Dedo", "Bebê",
-            "Anel"};
-
-        if (1 > 0) {
-            return;
-        }
-
         // avaliacao 15 está toda correta, vou usar essa agora para fazer a simulação sem muita complexidade
         String query = "SELECT "
                 + "avaliacaopalavra.id_avaliacao, "
@@ -107,23 +86,14 @@ public class Main {
             lines++;
         }
         System.out.println("lines read: " + lines + " cases in the assessment: " + assessment.getCases().size());
-        if (lines != 84 && lines != assessment.getCases().size()) {
-            System.out.println("Invalid assessment to do the simulation. All words are required.");
+
+        if (lines == 84 && lines == assessment.getCases().size()) {
+            SimulationWordsSequence simulation = new SimulationWordsSequence();
+
+            simulation.runSimulation(assessment, new SimulationWordsSequence.HarderWordsFirst());
+            simulation.runSimulation(assessment, new SimulationWordsSequence.EasiestWordsFirst());
         } else {
-            Map<Phoneme, Integer> mapCounter = new HashMap<>();
-
-            assessment.getCases().forEach(c -> {
-                System.out.println("\tcase of " + c.getWord());
-                c.getPhonemes().forEach(p -> {
-                    int count = 1;
-                    if (mapCounter.containsKey(p)) {
-                        count = mapCounter.get(p) + 1;
-                    }
-
-                    System.out.println(p + " -> " + count);
-                    mapCounter.put(p, count);
-                });
-            });
+            System.out.println("Invalid assessment to do the simulation. All words are required.");
         }
     }
 }
---
+++ src/main/java/br/com/efono/util/SimulationWordsSequence.java
@@ -0,0 +1,156 @@
+package br.com.efono.util;
+
+import br.com.efono.model.Assessment;
+import br.com.efono.model.KnownCase;
+import br.com.efono.model.Phoneme;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author João Bolsson (joaovictorbolsson@gmail.com)
+ * @version 2023, Jun 26.
+ */
+public class SimulationWordsSequence {
+
+    /**
+     * The first words in the array are easiest ones, according with the statistics in our database. The number of wrong
+     * transcriptions of a word indicates the level of difficult.
+     */
+    public static final String[] SORTED_WORDS = new String[]{"Anel", "Bebê", "Dedo", "Cama", "Batom", "Dado", "Navio", "Terra",
+        "Tênis", "Dente", "Rabo", "Faca", "Caminhão", "Fogo", "Sapo", "Sapato", "Cabelo", "Vaca", "Lápis", "Galinha",
+        "Mesa", "Gato", "Cavalo", "Bolsa", "Casa", "Sofá", "Barriga", "Calça", "Folha", "Chapéu", "Pastel", "Coelho",
+        "Língua", "Chinelo", "Beijo", "Caixa", "Cachorro", "Espelho", "Nuvem", "Relógio", "Porta", "Ventilador", "Zero",
+        "Passarinho", "Tesoura", "Nariz", "Jornal", "Garfo", "Jacaré", "Girafa", "Brinco", "Prato", "Cobra", "Zebra",
+        "Grama", "Cruz", "Trem", "Soprar", "Placa", "Fruta", "Fralda", "Refri", "Presente", "Livro", "Chiclete",
+        "Chifre", "Bicicleta", "Gritar", "Bruxa", "Letra", "Plástico", "Igreja", "Flor", "Escrever", "Dragão", "Magro",
+        "Estrela", "Pedra", "Vidro", "Microfone", "Colher", "Floresta", "Biblioteca", "Travesseiro"};
+
+    // TODO: tests
+    /**
+     * Runs the simulation with the words sequence in the given assessment.
+     *
+     * @param assessment Assessment.
+     * @param comp Comparator to sort KnownCases or null.
+     */
+    public void runSimulation(final Assessment assessment, final Comparator<KnownCase> comp) {
+        System.out.println("Running simulation with assessment with " + assessment.getCases().size() + " cases with " + comp + " comparator");
+        final Map<Phoneme, Integer> mapCounter = new HashMap<>();
+
+        List<KnownCase> cases = assessment.getCases();
+        if (comp != null) {
+            cases.sort(comp);
+        }
+
+        final List<String> wordsRequired = new LinkedList<>();
+
+        for (KnownCase c : cases) {
+            // TODO: ao inves de "fonemas produzidos" depois vai ser preciso pegar de algum gabarito os "fonemas alvo", pois são esses que estão sendo testados.
+            // os "fonemas produzidos" aqui precisam ser testados no mínimo 2 vezes para serem considerados "adquiridos" no inv. fonético. [esse é o trabalho da simulação]
+            // ou seja, ver qual o impacto que a sequência da avaliação tem sobre o inv. fonético.
+            // TODO: adicionar um c.getPhonemesRequired//target. Isso vai ser útil para fazer o PCC-R depois.
+            System.out.println("\tcase of " + c.getWord() + " phonemes produced: " + c.getPhonemes().size());
+            for (Phoneme p : c.getPhonemes()) {
+                int count = 1;
+                if (mapCounter.containsKey(p)) {
+                    count = mapCounter.get(p) + 1;
+                }
+
+                if (count <= 2) { // minumum of tests required
+                    System.out.println(p + " -> " + count);
+                    mapCounter.put(p, count);
+
+                    System.out.println("word required, " + p + " -> " + count);
+                    if (!wordsRequired.contains(c.getWord())) {
+                        wordsRequired.add(c.getWord());
+                    }
+                }
+                System.out.println("-----");
+            }
+
+            // check map counter
+            Iterator<Map.Entry<Phoneme, Integer>> iterator = mapCounter.entrySet().iterator();
+
+            int notOk = 0, ok = 0;
+            while (iterator.hasNext()) {
+                Map.Entry<Phoneme, Integer> next = iterator.next();
+
+                if (next.getValue() < 2) {
+                    notOk++;
+                } else {
+                    ok++;
+                }
+            }
+            System.out.println("phonemes test: not ok: " + notOk + " ok: " + ok);
+        }
+
+        System.out.println("--------------\nResult");
+        Iterator<Map.Entry<Phoneme, Integer>> iterator = mapCounter.entrySet().iterator();
+
+        int notOk = 0, ok = 0;
+        while (iterator.hasNext()) {
+            Map.Entry<Phoneme, Integer> next = iterator.next();
+
+            if (next.getValue() < 2) {
+                System.out.println("not ok: " + next.getKey() + " -> " + next.getValue());
+                notOk++;
+            } else {
+                ok++;
+            }
+        }
+
+        System.out.println("words required: " + wordsRequired.size() + ": " + wordsRequired);
+        float notOkcent = notOk * 100 / mapCounter.size();
+        float okcent = ok * 100 / mapCounter.size();
+
+        System.out.println("finish\n-------------\nnot ok: " + notOkcent + "% ok: " + okcent + "%");
+    }
+
+    
+    // TODO: comparator com indices misturados (busca binaria).
+    /**
+     * Sort KnownCases with harder words first.
+     */
+    public static class HarderWordsFirst implements Comparator<KnownCase> {
+
+        @Override
+        public int compare(final KnownCase o1, final KnownCase o2) {
+            // TODO: ignore case and acentuation
+            int indexOfo1 = Arrays.asList(SORTED_WORDS).indexOf(o1.getWord());
+            int indexOfo2 = Arrays.asList(SORTED_WORDS).indexOf(o2.getWord());
+            return indexOfo2 - indexOfo1;
+        }
+
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName();
+        }
+
+    }
+
+    /**
+     * Sort KnownCases with easiest words first.
+     */
+    public static class EasiestWordsFirst implements Comparator<KnownCase> {
+
+        @Override
+        public int compare(final KnownCase o1, final KnownCase o2) {
+            // TODO: ignore case and acentuation
+            int indexOfo1 = Arrays.asList(SORTED_WORDS).indexOf(o1.getWord());
+            int indexOfo2 = Arrays.asList(SORTED_WORDS).indexOf(o2.getWord());
+            return indexOfo1 - indexOfo2;
+        }
+
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName();
+        }
+
+    }
+
+}
