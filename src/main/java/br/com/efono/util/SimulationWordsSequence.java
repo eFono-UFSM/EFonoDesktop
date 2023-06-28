438d772a9d62fb07116849e2ff5c9fd618976e4b
João Bolsson <joaobolsson@animati.com.br>
2023-06-28 07:25:34 -0300

    removing some logs

--- src/main/java/br/com/efono/util/SimulationWordsSequence.java
+++ src/main/java/br/com/efono/util/SimulationWordsSequence.java
@@ -22,14 +22,15 @@ public class SimulationWordsSequence {
      * The first words in the array are easiest ones, according with the statistics in our database. The number of wrong
      * transcriptions of a word indicates the level of difficult.
      */
-    public static final String[] SORTED_WORDS = new String[]{"Anel", "Bebê", "Dedo", "Cama", "Batom", "Dado", "Navio", "Terra",
-        "Tênis", "Dente", "Rabo", "Faca", "Caminhão", "Fogo", "Sapo", "Sapato", "Cabelo", "Vaca", "Lápis", "Galinha",
-        "Mesa", "Gato", "Cavalo", "Bolsa", "Casa", "Sofá", "Barriga", "Calça", "Folha", "Chapéu", "Pastel", "Coelho",
-        "Língua", "Chinelo", "Beijo", "Caixa", "Cachorro", "Espelho", "Nuvem", "Relógio", "Porta", "Ventilador", "Zero",
-        "Passarinho", "Tesoura", "Nariz", "Jornal", "Garfo", "Jacaré", "Girafa", "Brinco", "Prato", "Cobra", "Zebra",
-        "Grama", "Cruz", "Trem", "Soprar", "Placa", "Fruta", "Fralda", "Refri", "Presente", "Livro", "Chiclete",
-        "Chifre", "Bicicleta", "Gritar", "Bruxa", "Letra", "Plástico", "Igreja", "Flor", "Escrever", "Dragão", "Magro",
-        "Estrela", "Pedra", "Vidro", "Microfone", "Colher", "Floresta", "Biblioteca", "Travesseiro"};
+    public static final String[] SORTED_WORDS = new String[]{"Anel", "Bebê", "Dedo", "Cama", "Batom", "Dado", "Navio",
+        "Terra", "Tênis", "Dente", "Rabo", "Faca", "Caminhão", "Fogo", "Sapo", "Sapato", "Cabelo", "Vaca", "Lápis",
+        "Galinha", "Mesa", "Gato", "Cavalo", "Bolsa", "Casa", "Sofá", "Barriga", "Calça", "Folha", "Chapéu", "Pastel",
+        "Coelho", "Língua", "Chinelo", "Beijo", "Caixa", "Cachorro", "Espelho", "Nuvem", "Relógio", "Porta",
+        "Ventilador", "Zero", "Passarinho", "Tesoura", "Nariz", "Jornal", "Garfo", "Jacaré", "Girafa", "Brinco",
+        "Prato", "Cobra", "Zebra", "Grama", "Cruz", "Trem", "Soprar", "Placa", "Fruta", "Fralda", "Refri", "Presente",
+        "Livro", "Chiclete", "Chifre", "Bicicleta", "Gritar", "Bruxa", "Letra", "Plástico", "Igreja", "Flor",
+        "Escrever", "Dragão", "Magro", "Estrela", "Pedra", "Vidro", "Microfone", "Colher", "Floresta", "Biblioteca",
+        "Travesseiro"};
 
     // TODO: tests
     /**
@@ -39,7 +40,9 @@ public class SimulationWordsSequence {
      * @param comp Comparator to sort KnownCases or null.
      */
     public void runSimulation(final Assessment assessment, final Comparator<KnownCase> comp) {
-        System.out.println("Running simulation with assessment with " + assessment.getCases().size() + " cases with " + comp + " comparator");
+        System.out.println("-----------------\n"
+                + "Running simulation in assessment with " + assessment.getCases().size()
+                + " cases with " + comp + " comparator");
         final Map<Phoneme, Integer> mapCounter = new HashMap<>();
 
         List<KnownCase> cases = assessment.getCases();
@@ -54,7 +57,7 @@ public class SimulationWordsSequence {
             // os "fonemas produzidos" aqui precisam ser testados no mínimo 2 vezes para serem considerados "adquiridos" no inv. fonético. [esse é o trabalho da simulação]
             // ou seja, ver qual o impacto que a sequência da avaliação tem sobre o inv. fonético.
             // TODO: adicionar um c.getPhonemesRequired//target. Isso vai ser útil para fazer o PCC-R depois.
-            System.out.println("\tcase of " + c.getWord() + " phonemes produced: " + c.getPhonemes().size());
+//            System.out.println("\tcase of " + c.getWord() + " phonemes produced: " + c.getPhonemes().size());
             for (Phoneme p : c.getPhonemes()) {
                 int count = 1;
                 if (mapCounter.containsKey(p)) {
@@ -62,15 +65,22 @@ public class SimulationWordsSequence {
                 }
 
                 if (count <= 2) { // minumum of tests required
-                    System.out.println(p + " -> " + count);
+//                    System.out.println(p + " -> " + count);
                     mapCounter.put(p, count);
 
-                    System.out.println("word required, " + p + " -> " + count);
+                    /**
+                     * If this word contains at least one phoneme which wasn't be tested at minimum two times, then the
+                     * word is important and will be "required".
+                     *
+                     * If all the phonemes tested by this word were already tested at minimum 2 times, so the word
+                     * doesn't would need to be here.
+                     */
+//                    System.out.println("word required, " + p + " -> " + count);
                     if (!wordsRequired.contains(c.getWord())) {
                         wordsRequired.add(c.getWord());
                     }
                 }
-                System.out.println("-----");
+//                System.out.println("-----");
             }
 
             // check map counter
@@ -86,10 +96,10 @@ public class SimulationWordsSequence {
                     ok++;
                 }
             }
-            System.out.println("phonemes test: not ok: " + notOk + " ok: " + ok);
+//            System.out.println("phonemes test: not ok: " + notOk + " ok: " + ok);
         }
 
-        System.out.println("--------------\nResult");
+        System.out.println("Result");
         Iterator<Map.Entry<Phoneme, Integer>> iterator = mapCounter.entrySet().iterator();
 
         int notOk = 0, ok = 0;
@@ -97,7 +107,7 @@ public class SimulationWordsSequence {
             Map.Entry<Phoneme, Integer> next = iterator.next();
 
             if (next.getValue() < 2) {
-                System.out.println("not ok: " + next.getKey() + " -> " + next.getValue());
+//                System.out.println("not ok: " + next.getKey() + " -> " + next.getValue());
                 notOk++;
             } else {
                 ok++;
@@ -108,10 +118,11 @@ public class SimulationWordsSequence {
         float notOkcent = notOk * 100 / mapCounter.size();
         float okcent = ok * 100 / mapCounter.size();
 
-        System.out.println("finish\n-------------\nnot ok: " + notOkcent + "% ok: " + okcent + "%");
+        System.out.println("less than the required two tests: " + notOkcent + "% at least two tests: " + okcent + "%");
+//        System.out.println("-------------------------------------------------------------------------");
     }
 
-    
+    // TODO: depois, simular a avaliação toda com o mesmo lance da busca binária, mas dessa vez, se o usuário acertou vai para uma mais difícil, se errou para mais fácil e assim por diante.
     // TODO: comparator com indices misturados (busca binaria).
     /**
      * Sort KnownCases with harder words first.
