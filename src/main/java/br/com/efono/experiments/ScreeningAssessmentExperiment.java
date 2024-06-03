721b595657cdc1133d88abff06166f50030a24c8
João Bolsson <joaobolsson@animati.com.br>
2024-06-03 15:37:58 -0300

    confusion matrix csv

--- src/main/java/br/com/efono/experiments/ScreeningAssessmentExperiment.java
+++ src/main/java/br/com/efono/experiments/ScreeningAssessmentExperiment.java
@@ -1,6 +1,7 @@
 package br.com.efono.experiments;
 
 import br.com.efono.model.Assessment;
+import br.com.efono.model.IndicatorInfo;
 import br.com.efono.util.DatabaseUtils;
 import br.com.efono.util.Defaults;
 import br.com.efono.util.FileUtils;
@@ -12,7 +13,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -63,6 +66,22 @@ public class ScreeningAssessmentExperiment {
         File parent = new File(outputDirectory, "Computer-Speech-2024-experiments-results");
         parent.mkdir();
 
+        // 0 -> negativo (não tem desvio - baixo,mod-baixo)
+        // 1 -> positivo (tem desvio - alto,mod-alto)
+        String[] realValues = new String[assessments.size()];
+
+        int maxWordsScreening = 5;
+        // Indicator Name -> predictedValues
+        Map<String, String[]> mapConfusionMatrix = new HashMap<>();
+
+        for (int i = 0; i <= maxWordsScreening; i++) {
+            // 0-> SDA
+            mapConfusionMatrix.put("Indicator " + i + "w", new String[assessments.size()]);
+        }
+
+        String negativo = "Negativo";
+        String positivo = "Positivo";
+
         System.out.println("Output directory with experiments results: " + parent);
 
         DecimalFormat df = new DecimalFormat("#.##");
@@ -71,19 +90,24 @@ public class ScreeningAssessmentExperiment {
 
             List<String> allWords = Arrays.asList(Defaults.SORTED_WORDS);
 
-            int maxWordsScreening = 5;
-
-            StringBuilder headerBuilder = new StringBuilder("ID_ASSESSMENT,PCC-R Complete Assessment,Indication 84w,");
-            for (int i = 1; i <= maxWordsScreening; i++) {
-                headerBuilder.append("Indication ").append(i).append("w,");
-            }
             // SDA (Screening Dynamic Assessment): no limits, continue until reaches a leaf node in the screening.
-            headerBuilder.append("Indication SDA\n");
+            StringBuilder headerBuilder = new StringBuilder("ID_ASSESSMENT,PCC-R Complete Assessment,Indicator 84w,Indicator SDA,");
+            for (int i = 1; i <= maxWordsScreening; i++) {
+                headerBuilder.append("Indicator ").append(i).append("w");
+                if (i < maxWordsScreening) {
+                    headerBuilder.append(",");
+                } else {
+                    headerBuilder.append("\n");
+                }
+            }
 
             writer.write(headerBuilder.toString());
-            for (Assessment a : assessments) {
+            for (int index = 0; index < assessments.size(); index++) {
+                Assessment a = assessments.get(index);
                 double pccr = a.getPCCR(allWords);
                 String indicatorPCCR = Util.getDegree(pccr);
+                realValues[index] = indicatorPCCR.contains("Low") ? negativo : positivo;
+
                 StringBuilder sb = new StringBuilder();
 
                 sb.append(a.getId()).append(",");
@@ -92,15 +116,69 @@ public class ScreeningAssessmentExperiment {
 
                 Range referenceRangeIndicatorPCCR = Util.getTreeIndicatorFromPCCR(indicatorPCCR);
 
-                for (int i = 1; i <= maxWordsScreening; i++) {
-                    int indicatorSDA = Util.getIndicatorSDA(a, i).getIndicator();
+                // 0 -> SDA
+                for (int i = 0; i <= maxWordsScreening; i++) {
+                    IndicatorInfo info = Util.getIndicatorSDA(a, i);
+                    int indicatorSDA = info.getIndicator();
                     int deltaFromSDA = getDelta(referenceRangeIndicatorPCCR, indicatorSDA);
-                    sb.append(deltaFromSDA).append(",");
+                    sb.append(deltaFromSDA);
+
+                    if (i < maxWordsScreening) {
+                        sb.append(",");
+                    } else {
+                        sb.append("\n");
+                    }
+
+                    // predicted values to build confusion matrix
+                    String[] predictedValues = mapConfusionMatrix.get("Indicator " + i + "w");
+                    predictedValues[index] = info.getIndicatorAsString().contains("Low") ? negativo : positivo;
+                    mapConfusionMatrix.put("Indicator " + i + "w", predictedValues);
                 }
-                // SDA
-                int deltaSDA = getDelta(referenceRangeIndicatorPCCR, Util.getIndicatorSDA(a, 0).getIndicator());
-                sb.append(deltaSDA).append("\n");
                 writer.write(sb.toString());
+            }
+        } catch (final IOException ex) {
+            System.out.println("Couldn't write into file: " + ex);
+        }
+
+        File confusionMatrixFile = new File(parent, "confusion-matrix.csv");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(confusionMatrixFile))) {
+            for (int index = 0; index <= maxWordsScreening; index++) {
+                String key = "Indicator " + index + "w";
+                writer.write("Confusion Matrix for " + key + "\n\n");
+
+                String[] predictedValues = mapConfusionMatrix.get(key);
+                /*
+                Verdadeiros positivos (TP): o número de previsões corretas para a classe "Positivo".
+                Verdadeiros negativos (TN): o número de previsões corretas para a classe "Negativo".
+                Falsos positivos (FP): o número de previsões "Positivo" que são na verdade "Negativo".
+                Falsos negativos (FN): o número de previsões "Negativo" que são na verdade "Positivo".
+                 */
+                double tp = 0, tn = 0, fp = 0, fn = 0;
+                if (realValues.length == predictedValues.length) {
+                    for (int i = 0; i < realValues.length; i++) {
+                        if (realValues[i].equals(positivo) && predictedValues[i].equals(positivo)) {
+                            tp++;
+                        } else if (realValues[i].equals(negativo) && predictedValues[i].equals(negativo)) {
+                            tn++;
+                        } else if (realValues[i].equals(negativo) && predictedValues[i].equals(positivo)) {
+                            fp++;
+                        } else if (realValues[i].equals(positivo) && predictedValues[i].equals(negativo)) {
+                            fn++;
+                        }
+                    }
+                }
+
+                writer.write(",Previsão Positivo,Previsão Negativo\n");
+                writer.write("Real Positivo," + tp + "," + fn + "\n");
+                writer.write("Real Negativo," + fp + "," + tn + "\n\n");
+
+                double sensibilidade = tp / (tp + fn);
+                double especificidade = tn / (tn + fp);
+                double acuracia = (tp + tn) / (tp + tn + fp + fn);
+
+                writer.write("Sensibilidade," + df.format(sensibilidade).replaceAll(",", ".") + "\n");
+                writer.write("Especificidade," + df.format(especificidade).replaceAll(",", ".") + "\n");
+                writer.write("Acurácia," + df.format(acuracia).replaceAll(",", ".") + "\n\n\n");
             }
         } catch (final IOException ex) {
             System.out.println("Couldn't write into file: " + ex);
